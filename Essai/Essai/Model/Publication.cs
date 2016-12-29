cf80ef8622f7f00f6b85aec9af548a6733da52d2
rahma12 <rahma.rosa08@gmail.com>
2016-12-29 08:33:26 -0800

    creation de model publication

---
+++ Essai/Essai/Model/Publication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essai.Model
+{
+    public class Publication
+    {
+        public String Id { get; set; }
+
+        public String statut { get; set; }
+
+        public String image { get; set; }
+
+        public int like { get; set; }
+
+        public int deslike { get; set; }
+
+        public String commente { get; set; }
+    }
+}
