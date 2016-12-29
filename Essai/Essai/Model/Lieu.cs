4d7204caa9ed7911a17282f01c9a1cb7a6aff1df
rahma <rahma.rosa08@gmail.com>
2016-12-29 17:37:01 +0100

    creation de model Lieu

---
+++ Essai/Essai/Model/Lieu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essai.Model
+{
+   public class Lieu
+    {
+        public String Id { get; set; }
+        public String nom { get; set; }
+        public String adresse { get; set; }
+        public String longitude{ get; set; }
+        public String lattitude { get; set; }
+
+    }
+}
